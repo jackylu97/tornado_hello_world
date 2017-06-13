80b34b6b8ba8abeacd0fd6049393503d3da60498
Jacky Lu <jackylu@berkeley.edu>
2017-06-13 11:43:27 -0700

    failed attempt

--- hello.py
+++ hello.py
@@ -6,17 +6,20 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
+class SearchHandler(tornado.web.RequestHandler):
+    def get(self, term):
+        url = "https://api.github.com/users/" + term + "/repos"
+        result = yield tornado.httpclient.HTTPRequest(url, method = "GET")
+        return result
+
 def make_app():
     return tornado.web.Application(
         [
-            (r"/", MainHandler)
+            (r"/", MainHandler),
+            (r"/search/(\w+)", SearchHandler)
         ],
         static_path=os.path.join(os.path.dirname(__file__), "static")
     )
-
-# class SearchHandler(tornado.web.RequestHandler):
-#     def post(self):
-#         self.write("asdf")
 
 
 if __name__ == "__main__":
--- static/hello.js
+++ static/hello.js
@@ -1,30 +1,81 @@
 $(document).ready(function() {
   $("#searchform").on("submit", function() {
-        user = $(this).children()[0].value;
-        repos = search(user);
-        return false;
+    var user = $("#username").val();
+    // var url = "https://api.github.com/users/" + user + "/repos?per_page=100";
+    var repo_arr = [];
+    var url = "https://api.github.com/users/" + user + "/repos?per_page=100";
+
+    $.when(get_url(url, repo_arr)).then(create_html_from_list(repo_arr), function() {
+      $("#list_repos").html("<span>Could not find the user you were looking for :(</span>");
     });
+
+    return false;
+  });
 
 });
 
-function search(username) {
-  rv = null
-  rv = $.get("https://api.github.com/users/" + username + "/repos", function(result) {
-    console.log(result);
-    textarea = $("#list_repos");
-    textarea.html("<span>Found!</span>");
-    var to_add = $("<ul>");
-    if($.isEmptyObject(result)) {
-      to_add.append($("<span>").text("This user has no repositories"));
+function get_url(url, repo_arr) {
+  return $.get(url, function(result, textStatus, jqXHR) {
+    for (var item in result) {
+      repo_arr.push(result[item]);
     }
-    for (item in result) {
-      list_item = $("<li>");
-      list_item.text(result[item]["name"]);
-      to_add.append(list_item);
+    var next = null;
+    var link = jqXHR.getResponseHeader("Link");
+    var rels = link.match(/"[^"]+"/g);
+    var urls = link.match(/<[^>]+>/g);
+    for (var i = 0; i < rels.length; i+=1) {
+      if (rels[i] == "next") {
+        next = urls[i];
+      }
     }
-    textarea.append(to_add);
-  }).fail( function(result) {
-    textarea = $("#list_repos");
-    textarea.html("<span>Could not find the user you were looking for :(</span>");
-  })
+    get_url(next, repo_arr);
+  }).fail( function() {
+
+  });
+}
+
+/*
+$(document).ready(function() {
+  $("#searchform").on("submit", function() {
+        $.get()
+    });
+
+});
+*/
+
+// function get_repos(username, successCb, failureCb) {
+//   var repo_arr = [];
+//   $.get("https://api.github.com/users/" + username + "/repos?per_page=100", function(result) {
+//     for (var item in result) {
+//       repo_arr.push(result[item]);
+//     }
+//     successCb(repo_arr);
+//   }).fail( function(result) {
+//     repo_arr = null;
+//     failureCb(result);
+//   });
+//   return repo_arr;
+// }
+
+
+
+function create_html_from_list(arr) {
+  console.log(arr);
+  var textarea = $("#list_repos");
+
+
+  textarea.html("<span>Found!</span>");
+
+  var unordered_list = $("<ul>");
+
+  if (arr.length == 0) {
+    unordered_list.append($("<span>").text("This user has no repositories"));
+  }
+
+  for (var item in arr) {
+    var list_item = $("<li>");
+    list_item.text(arr[item]["name"]);
+    unordered_list.append(list_item);
+  }
+  textarea.append(unordered_list);
 }
